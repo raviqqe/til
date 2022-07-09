7ba7020053b3ab63fe7681688d066920a6a2cc7d
Yota Toyama <raviqqe@gmail.com>
2022-07-09 23:39:15 +0000

    Update

--- llvm/spill_large_struct.c
+++ llvm/spill_large_struct.c
@@ -7,5 +7,10 @@ struct foo_t {
 
 uint64_t foo(uint64_t x1, uint64_t x2, uint64_t x3, uint64_t x4, uint64_t x5,
              struct foo_t x6, uint64_t x7, uint64_t x8) {
-  return x5.x2;
+  return x5 + x6.x1;
+}
+
+uint64_t bar(uint64_t x1, uint64_t x2, uint64_t x3, uint64_t x4,
+             struct foo_t x5, uint64_t x6, uint64_t x7, uint64_t x8) {
+  return x5.x2 + x6;
 }
