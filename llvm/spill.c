2cf6d2aa71c143365928fbbc4f2ef6b36659a12a
Yota Toyama <raviqqe@gmail.com>
2022-07-09 23:21:21 +0000

    Update

--- llvm/spill.c
+++ llvm/spill.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 
 uint64_t foo(uint64_t x1, uint64_t x2, uint64_t x3, uint64_t x4, uint64_t x5,
-             uint64_t x6, uint64_t x7, uint64_t x8) {
-  return x8;
+             uint64_t x6, uint64_t x7, uint64_t x8, uint64_t x9, uint64_t x10) {
+  return x7;
 }
