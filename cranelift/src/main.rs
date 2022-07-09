1f1506205530ca57f5773f81a4d8c8a305391a0a
Yota Toyama <raviqqe@gmail.com>
2022-07-09 03:05:25 +0000

    Update

--- cranelift/src/main.rs
+++ cranelift/src/main.rs
@@ -1,3 +1,23 @@
+use cranelift::codegen::isa;
+use cranelift::codegen::settings::{self, Flags};
+use cranelift::prelude::Configurable;
+use cranelift_object::ObjectBuilder;
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    run().unwrap();
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let mut builder = settings::builder();
+
+    builder.set("opt_level", "speed_and_size")?;
+
+    let flags = Flags::new(builder);
+    let builder = isa::lookup_by_name("x86_64-unknown-linux-gnu")?;
+
+    builder.finish(flags)?;
+    // ObjectBuilder::new("foo");
+
+    Ok(())
 }
