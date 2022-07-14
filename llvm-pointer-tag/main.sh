ce5d09e37aafff60020cfc2df0056cfa94d37e1c
Yota Toyama <raviqqe@gmail.com>
2022-07-14 04:40:43 +0000

    Fix

--- llvm-pointer-tag/main.sh
+++ llvm-pointer-tag/main.sh
@@ -6,6 +6,6 @@ brew install llvm
 
 llc -O3 main.ll
 
-for options in '-o main_c.s' '--emit-llvm -o main_c.ll'; do
+for options in '-o main_c.s' '-emit-llvm -o main_c.ll'; do
   clang -c -S -O3 $options main.c
 done
