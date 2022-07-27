a05f671097f9d8133f769d8514aa3673ba9ace15
Yota Toyama <raviqqe@gmail.com>
2022-07-27 15:30:06 +0000

    Add tokio

--- rust-stream/src/main.rs
+++ rust-stream/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use async_stream::stream;
+use futures::{Stream, StreamExt};
+use tokio_stream::StreamMap;
+
+#[tokio::main]
+async fn main() {
+    let mut stream = StreamMap::from_iter(
+        [create_stream(1), create_stream(2)]
+            .into_iter()
+            .enumerate()
+            .map(|(index, stream)| (index, Box::pin(stream))),
+    );
+
+    while let Some(element) = stream.next().await {
+        println!("{:?}", element);
+    }
+}
+
+fn create_stream(number: usize) -> impl Stream<Item = usize> {
+    stream! {
+        loop {
+            yield number;
+        }
+    }
 }
