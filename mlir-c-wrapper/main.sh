ebd2a753b1e571cc7881be091a67ec6916d2efdd
Yota Toyama <raviqqe@gmail.com>
2022-10-12 18:34:15 -0700

    Fix MLIR C wrapper emission (#18)

--- mlir-c-wrapper/main.sh
+++ mlir-c-wrapper/main.sh
@@ -6,4 +6,7 @@ brew install llvm@15
 
 export PATH=$(brew --prefix llvm@15)/bin:$PATH
 
-mlir-translate --mlir-to-llvmir -o foo.ll foo.mlir
+mkdir -p tmp
+
+mlir-reduce --convert-func-to-llvm foo.mlir >tmp/foo.mlir
+mlir-translate --mlir-to-llvmir -o tmp/foo.ll tmp/foo.mlir
