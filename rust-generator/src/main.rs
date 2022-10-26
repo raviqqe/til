0071a8c6a72821c4e38a238a8b11660325dc7aae
Yota Toyama <raviqqe@gmail.com>
2022-10-25 18:23:15 -0700

    Fix

--- rust-generator/src/main.rs
+++ rust-generator/src/main.rs
@@ -1,3 +1,16 @@
+#![feature(generators, generator_trait)]
+
+use std::{ops::Generator, pin::Pin};
+
 fn main() {
-    println!("Hello, world!");
+    let x = Box::new(1);
+    let y = Box::new(2);
+
+    let mut generate = || {
+        yield x;
+        yield y;
+    };
+
+    dbg!(Pin::new(&mut generate).resume(()));
+    dbg!(Pin::new(&mut generate).resume(()));
 }
