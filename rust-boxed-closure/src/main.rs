5f97a646ee9cabce2ada6585ac8b48bdbfbc0ea2
Yota Toyama <raviqqe@gmail.com>
2022-11-20 12:43:50 -0800

    Fix

--- rust-boxed-closure/src/main.rs
+++ rust-boxed-closure/src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
     let foo: Box<dyn Fn(u64) -> u64> = Box::new(|x| x);
-    let bar = Box::<dyn Fn(u64) -> u64>::new(|x| x);
+
+    println!("{}", foo(42));
+
+    // This results in a compile error!
+    // let bar = Box::<dyn Fn(u64) -> u64>::new(|x| x);
 }
