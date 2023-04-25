1645a0e8bff31a26e0d9b83f19355027425a7dae
Yota Toyama <raviqqe@gmail.com>
2023-04-25 17:09:07 +1000

    Reproduce wildcard exports bug

---
+++ webpack-wildcard-exports/webpack.config.js
@@ -0,0 +1,20 @@
+import { resolve } from "node:path";
+
+export default {
+  mode: "production",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts"],
+    extensionAlias: {
+      ".js": [".ts", ".js"],
+    },
+  },
+};
