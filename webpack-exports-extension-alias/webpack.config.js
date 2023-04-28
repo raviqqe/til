a5954048c3cf8a5561e25ad1306aff9853eeff73
Yota Toyama <raviqqe@gmail.com>
2023-04-28 10:42:48 +1000

    Fix

--- webpack-exports-extension-alias/webpack.config.js
+++ webpack-exports-extension-alias/webpack.config.js
@@ -1,14 +1,6 @@
 export default {
   mode: "production",
   entry: "./src/index.js",
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-      },
-    ],
-  },
   resolve: {
     extensions: [".js"],
     extensionAlias: {
