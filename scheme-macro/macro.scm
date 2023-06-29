d25d3351eb1cd36d86a4d64632032a68613a31aa
Yota Toyama <raviqqe@gmail.com>
2023-06-29 15:16:36 +1000

    Use define-syntax

--- scheme-macro/macro.scm
+++
@@ -1,8 +0,0 @@
-(define (f x)
-  (+ x x))
-
-(define (i x) x)
-
-(defmacro (m x) x)
-
-(print (m (f 5)))
---
+++ scheme-macro/main.scm
@@ -0,0 +1,9 @@
+(define (f x) (+ x x))
+
+(define (i x) x)
+
+(define-syntax call-me-maybe
+  (syntax-rules ()
+		((call-me-maybe f x) (f x))))
+
+(display (call-me-maybe f 42))
