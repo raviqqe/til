a94809636f11fef7ac32ba4241104351dbfb4785
Yota Toyama <raviqqe@gmail.com>
2023-12-09 23:59:48 +0900

    Cloudflare on Terraform (#48)

---
+++ terraform-cloudflare/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "cloudflare" {}
+
+data "cloudflare_accounts" "main" {}
+
+locals {
+  cloudflare_account = data.cloudflare_accounts.main.accounts[0]
+}
+
+resource "cloudflare_workers_kv_namespace" "production" {
+  account_id = local.cloudflare_account.id
+  title      = "strawberry"
+}
+
+data "cloudflare_api_token_permission_groups" "all" {}
+
+resource "cloudflare_api_token" "audit_logs" {
+  name = "logs_account"
+
+  policy {
+    permission_groups = [
+      data.cloudflare_api_token_permission_groups.all.account["Access: Audit Logs Read"],
+    ]
+    resources = {
+      "com.cloudflare.api.account.${local.cloudflare_account.id}" = "*"
+    }
+  }
+}
+
+output "account_premission_groups" {
+  value = data.cloudflare_api_token_permission_groups.all.account
+}
+
+output "user_premission_groups" {
+  value = data.cloudflare_api_token_permission_groups.all.user
+}
