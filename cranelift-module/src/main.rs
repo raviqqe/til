3b0c664ce0d005e0a804a5946ed20b7badb9dea7
Yota Toyama <raviqqe@gmail.com>
2023-12-10 00:21:54 +0900

    Fix

--- cranelift-module/src/main.rs
+++ cranelift-module/src/main.rs
@@ -114,9 +114,8 @@ fn compile_function(function: &mut Function) -> Result<(), Box<dyn Error>> {
     }
     {
         let arg = builder.use_var(y);
-        builder.ins().brnz(arg, block3, &[]);
+        builder.ins().brif(arg, block3, &[], block2, &[]);
     }
-    builder.ins().jump(block2, &[]);
 
     builder.switch_to_block(block2);
     builder.seal_block(block2);
