4c39a59c0d006d46f83c340e6935d9b22be73df9
Yota Toyama <raviqqe@gmail.com>
2024-01-14 11:55:45 +0900

    Fix

--- rust-fibonacci/src/lib.rs
+++ rust-fibonacci/src/lib.rs
@@ -14,5 +14,10 @@ mod tests {
     fn calculate() {
         assert_eq!(fibonacci(0), 0);
         assert_eq!(fibonacci(1), 1);
+        assert_eq!(fibonacci(2), 1);
+        assert_eq!(fibonacci(3), 2);
+        assert_eq!(fibonacci(4), 3);
+        assert_eq!(fibonacci(5), 5);
+        assert_eq!(fibonacci(6), 8);
     }
 }
