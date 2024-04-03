b72bd96fa91e626ff3ca05f6e92caf6444f66811
Yota Toyama <raviqqe@gmail.com>
2024-04-03 23:17:13 +0900

    Fix

---
+++ rust-libc/src/main.rs
@@ -0,0 +1,11 @@
+#![no_std]
+#![cfg_attr(not(test), no_main)]
+
+extern crate libc;
+
+#[cfg_attr(not(test), no_mangle)]
+unsafe extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
+    libc::printf(b"Hello, World!\n".as_ptr() as *const i8);
+
+    0
+}
