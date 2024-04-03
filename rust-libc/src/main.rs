8f1c6f498a3695f85b888d1054828a50771bed71
Yota Toyama <raviqqe@gmail.com>
2024-04-03 23:54:54 +0900

    Minimal hello world (#99)

---
+++ rust-libc/src/main.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![cfg_attr(not(test), no_main)]
+
+#[cfg_attr(not(test), no_mangle)]
+unsafe extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
+    libc::printf(b"Hello, World!\n".as_ptr() as _);
+
+    0
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { libc::exit(1) }
+}
