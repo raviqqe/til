e7c9ac5dd0545c124af2b6d7605b15df51bf99fe
Yota Toyama <raviqqe@gmail.com>
2024-04-05 23:33:30 +0900

    Fix

--- small-integer-huffman-coding/src/lib.rs
+++ small-integer-huffman-coding/src/lib.rs
@@ -1,3 +1,21 @@
-pub fn compress(input: &[u8], output: &mut Vec<u8>) {
-    println!("Hello, world!");
+const SIZE: usize = 8;
+
+pub fn encode(input: &[u8], output: &mut Vec<u8>) {
+    let mut y = 0usize;
+    let mut offset = 0;
+
+    for &x in input {
+        match x {
+            0 => {
+                y <<= 1;
+                offset += 1;
+            }
+            _ => todo!(),
+        }
+
+        if offset >= SIZE {
+            output.push((y % (1 << SIZE)) as u8);
+            offset -= SIZE;
+        }
+    }
 }
