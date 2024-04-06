ec52025ceefe8dd946a36b130230d8dbfac259ea
Yota Toyama <raviqqe@gmail.com>
2024-04-06 12:39:44 +0900

    Fix

--- small-integer-huffman-coding/src/lib.rs
+++ small-integer-huffman-coding/src/lib.rs
@@ -33,6 +33,39 @@ pub fn encode(xs: &[u8]) -> Vec<u8> {
     ys
 }
 
+pub fn decode(xs: &[u8]) -> Vec<u8> {
+    let mut ys = vec![];
+    let mut y = 0usize;
+    let mut offset = 0;
+
+    for &x in xs {
+        match x {
+            0 => {
+                offset += 1;
+                y <<= 1;
+            }
+            1 => {
+                offset += 2;
+                y <<= 2;
+                y += 1;
+            }
+            _ => todo!(),
+        }
+
+        if offset >= SIZE {
+            ys.push(y as u8);
+            y <<= SIZE;
+            offset -= SIZE;
+        }
+    }
+
+    if offset > 0 {
+        ys.push(y as u8);
+    }
+
+    ys
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -60,5 +93,30 @@ mod tests {
         fn encode_one() {
             assert_eq!(encode(&[1]), &[1]);
         }
+    }
+
+    mod decode {
+        use super::*;
+        use pretty_assertions::assert_eq;
+
+        #[test]
+        fn decode_nothing() {
+            assert_eq!(decode(&encode(&[])), &[]);
+        }
+
+        // #[test]
+        // fn decode_zero() {
+        //     assert_eq!(decode(&[0]), &[0]);
+        // }
+
+        // #[test]
+        // fn decode_two_zeros() {
+        //     assert_eq!(decode(&[0, 0]), &[0]);
+        // }
+
+        // #[test]
+        // fn decode_one() {
+        //     assert_eq!(encode(&[1]), &[1]);
+        // }
     }
 }
