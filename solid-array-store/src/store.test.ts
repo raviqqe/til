d905fb143f130b6d0e100493c5efdcdc974d2562
Yota Toyama <raviqqe@gmail.com>
2024-06-06 21:43:03 +0900

    Fix

--- solid-array-store/src/store.test.ts
+++ solid-array-store/src/store.test.ts
@@ -1,6 +1,16 @@
 import { createStore, reconcile } from "solid-js/store";
 import { expect, it } from "vitest";
 
+it("swaps an object with an array", () => {
+  const [store, setStore] = createStore<{ value: {} | [] }>({
+    value: {},
+  });
+
+  setStore(reconcile({ value: [] }));
+
+  expect(Array.isArray(store.value)).toBe(true);
+});
+
 it("swaps an array with an object", () => {
   const [store, setStore] = createStore<{ value: {} | [] }>({
     value: [],
@@ -8,5 +18,5 @@ it("swaps an array with an object", () => {
 
   setStore(reconcile({ value: {} }));
 
-  expect(Array.isArray(store.value)).toBe(true);
+  expect(Array.isArray(store.value)).toBe(false);
 });
