68d707f1f3f3e4eadea6a3327053b738b94168f3
Yota Toyama <raviqqe@gmail.com>
2024-06-06 21:45:22 +0900

    Fix

--- solid-array-store/src/store.test.ts
+++ solid-array-store/src/store.test.ts
@@ -20,3 +20,23 @@ it("swaps an array with an object", () => {
 
   expect(Array.isArray(store.value)).toBe(false);
 });
+
+it("reconciles an object with an array", () => {
+  const [store, setStore] = createStore<{ value: {} | [] }>({
+    value: { foo: "bar" },
+  });
+
+  setStore("value", reconcile([0, 1, 2]));
+
+  expect(Array.isArray(store.value)).toBe(true);
+});
+
+it("swaps an array with an object", () => {
+  const [store, setStore] = createStore<{ value: {} | [] }>({
+    value: [0, 1, 2],
+  });
+
+  setStore("value", reconcile({ foo: "bar" }));
+
+  expect(Array.isArray(store.value)).toBe(false);
+});
