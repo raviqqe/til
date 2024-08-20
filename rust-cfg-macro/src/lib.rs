f1b24226d53a8ed75f17e39745e253ec03f31bfe
Yota Toyama <raviqqe@gmail.com>
2024-08-20 21:31:44 +0900

    Fix

--- rust-cfg-macro/src/lib.rs
+++ rust-cfg-macro/src/lib.rs
@@ -4,10 +4,10 @@
 macro_rules! feature {
     (if $name:literal { $then:expr } else { $else:expr }) => {
         (|| {
-                                                                    #[cfg(feature = $name)]
-                                                                    return $then;
-                                                                    #[cfg(not(feature = $name))]
-                                                                    return $else;
-                                                                }())
+            #[cfg(feature = $name)]
+            return $then;
+            #[cfg(not(feature = $name))]
+            return $else;
+        })()
     };
 }
--- rust-cfg-macro/tests/feature.rs
+++ rust-cfg-macro/tests/feature.rs
@@ -4,5 +4,5 @@ use rust_cfg_macro::feature;
 fn expression() {
     let x = feature!(if "test" { 42 } else { 13 });
 
-    assert_eq!(x);
+    assert_eq!(x, 42);
 }
