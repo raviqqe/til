7260b4138236f8f28cacd388042bc828b0c43c17
Yota Toyama <raviqqe@gmail.com>
2024-08-20 21:32:05 +0900

    Fix

--- rust-cfg-macro/tests/feature.rs
+++ rust-cfg-macro/tests/feature.rs
@@ -2,7 +2,7 @@ use rust_cfg_macro::feature;
 
 #[test]
 fn expression() {
-    let x = feature!(if "test" { 42 } else { 13 });
+    let x = feature!(if "foo" { 13 } else { 42 });
 
     assert_eq!(x, 42);
 }
