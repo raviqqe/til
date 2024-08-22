6d0d585d33a07ea074eacee8ff05e5d3ec514263
Yota Toyama <raviqqe@gmail.com>
2024-08-22 20:10:01 +0900

    Fix

--- rust-float-decomposition/src/lib.rs
+++ rust-float-decomposition/src/lib.rs
@@ -1,4 +1,4 @@
-pub fn decompose(x: f64) -> (i64, i64) {
+pub fn decompose_simple(x: f64) -> (i64, i64) {
     let y = (x.log2() - 52.0).max(0.0);
 
     ((x / y.exp2()) as _, y as _)
@@ -9,16 +9,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {
-        assert_eq!(decompose(1.0), (1, 0));
-        assert_eq!(decompose(2.0), (2, 0));
-        assert_eq!(decompose(52.0f64.exp2()), (4503599627370496, 0));
-        assert_eq!(decompose(53.0f64.exp2()), (4503599627370496, 1));
-        assert_eq!(decompose(54.0f64.exp2()), (4503599627370496, 2));
-        assert_eq!(decompose(55.0f64.exp2()), (4503599627370496, 3));
-        assert_eq!(decompose(56.0f64.exp2()), (4503599627370496, 4));
+    fn simple() {
+        assert_eq!(decompose_simple(1.0), (1, 0));
+        assert_eq!(decompose_simple(2.0), (2, 0));
+        assert_eq!(decompose_simple(52.0f64.exp2()), (4503599627370496, 0));
+        assert_eq!(decompose_simple(53.0f64.exp2()), (4503599627370496, 1));
+        assert_eq!(decompose_simple(54.0f64.exp2()), (4503599627370496, 2));
+        assert_eq!(decompose_simple(55.0f64.exp2()), (4503599627370496, 3));
+        assert_eq!(decompose_simple(56.0f64.exp2()), (4503599627370496, 4));
         assert_eq!(
-            decompose((52.0f64.exp2() + 1.0) * 4.0f64.exp2()),
+            decompose_simple((52.0f64.exp2() + 1.0) * 4.0f64.exp2()),
             (4503599627370497, 4)
         );
     }
