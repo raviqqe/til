4e028be59d2c137e29d057cc1d3719ae78dcf02e
Yota Toyama <raviqqe@gmail.com>
2024-08-22 20:25:04 +0900

    Fix

--- rust-float-decomposition/src/lib.rs
+++ rust-float-decomposition/src/lib.rs
@@ -1,6 +1,18 @@
 pub fn decompose_simple(x: f64) -> (i64, i64) {
-    let y = (x.log2() - 52.0).max(0.0);
+    calculate_parts(x, (x.log2() - 52.0).max(0.0).floor())
+}
 
+pub fn decompose_normal(x: f64) -> (i64, i64) {
+    let mut y = x.log2();
+
+    while (x / y.floor().exp2()).fract() > f64::EPSILON {
+        y -= 1.0;
+    }
+
+    calculate_parts(x, y.floor())
+}
+
+fn calculate_parts(x: f64, y: f64) -> (i64, i64) {
     ((x / y.exp2()) as _, y as _)
 }
 
@@ -12,6 +24,7 @@ mod tests {
     fn simple() {
         assert_eq!(decompose_simple(1.0), (1, 0));
         assert_eq!(decompose_simple(2.0), (2, 0));
+        assert_eq!(decompose_simple(123.0), (123, 0));
         assert_eq!(decompose_simple(52.0f64.exp2()), (4503599627370496, 0));
         assert_eq!(decompose_simple(53.0f64.exp2()), (4503599627370496, 1));
         assert_eq!(decompose_simple(54.0f64.exp2()), (4503599627370496, 2));
@@ -21,5 +34,12 @@ mod tests {
             decompose_simple((52.0f64.exp2() + 1.0) * 4.0f64.exp2()),
             (4503599627370497, 4)
         );
+    }
+
+    #[test]
+    fn normal() {
+        assert_eq!(decompose_normal(1.0), (1, 0));
+        assert_eq!(decompose_normal(2.0), (1, 1));
+        assert_eq!(decompose_normal(123.0), (123, 0));
     }
 }
