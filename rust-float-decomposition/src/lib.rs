7a700394e34e2c3aaeca4dbdd0e49bba4dd676d1
Yota Toyama <raviqqe@gmail.com>
2024-08-26 21:17:56 +0900

    Fix

--- rust-float-decomposition/src/lib.rs
+++ rust-float-decomposition/src/lib.rs
@@ -3,3 +3,21 @@ mod decompose;
 
 pub use compose::*;
 pub use decompose::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! assert_float {
+        ($x:literal) => {
+            let (x, y) = decompose_normal($x);
+
+            assert_eq!(compose(x, y), $x);
+        };
+    }
+
+    #[test]
+    fn simple() {
+        assert_float!(0.0);
+    }
+}
