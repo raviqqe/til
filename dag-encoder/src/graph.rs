90f838c8ac354a783613da9f2b73c6bf24e91d2e
Yota Toyama <raviqqe@gmail.com>
2024-09-19 19:36:31 +0900

    Fix

--- dag-encoder/src/graph.rs
+++ dag-encoder/src/graph.rs
@@ -1,21 +1,17 @@
 use crate::Node;
+use std::rc::Rc;
 
 #[derive(Debug, PartialEq)]
 pub struct Graph {
-    values: Vec<f64>,
-    nodes: Vec<Node>,
+    root: Rc<Node>,
 }
 
 impl Graph {
-    pub fn new(values: Vec<f64>, nodes: Vec<Node>) -> Self {
-        Self { values, nodes }
+    pub fn new(root: Rc<Node>) -> Self {
+        Self { root }
     }
 
-    pub fn values(&self) -> impl Iterator<Item = f64> + '_ {
-        self.values.iter().copied()
-    }
-
-    pub fn nodes(&self) -> impl Iterator<Item = &Node> {
-        self.nodes.iter()
+    pub fn root(&self) -> &Rc<Node> {
+        &self.root
     }
 }
