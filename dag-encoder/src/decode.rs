0c5e93103de2cfa72b29819bdf7a905329e493a1
Yota Toyama <raviqqe@gmail.com>
2024-09-19 19:56:09 +0900

    Fix

--- dag-encoder/src/decode.rs
+++ dag-encoder/src/decode.rs
@@ -1,1 +1,14 @@
-pub fn decode() {}
+pub fn decode(codes: &[u8]) -> Result<Graph, &'static str> {
+    Ok(Graph::default())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use insta::assert_debug_snapshot;
+
+    #[test]
+    fn encode_empty() {
+        assert_debug_snapshot!(decode(&[]));
+    }
+}
