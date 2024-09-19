fdebebd42d8fc6f44c3c86f696b205e3582846de
Yota Toyama <raviqqe@gmail.com>
2024-09-19 19:39:19 +0900

    Fix

---
+++ dag-encoder/src/encode.rs
@@ -0,0 +1,4 @@
+use crate::Graph;
+use std::io::Write;
+
+pub fn encode(_graph: &Graph, _writer: &impl Write) {}
--- dag-encoder/src/lib.rs
+++ dag-encoder/src/lib.rs
@@ -1,7 +1,11 @@
+mod decode;
+mod encode;
 mod graph;
 mod node;
 mod payload;
 
+pub use decode::decode;
+pub use encode::encode;
 pub use graph::Graph;
 pub use node::Node;
 pub use payload::Payload;
