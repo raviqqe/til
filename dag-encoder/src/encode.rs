67a28161d49f3c7c5353e47b4d262d9ecbeb7158
Yota Toyama <raviqqe@gmail.com>
2024-09-19 19:52:04 +0900

    Fix

--- dag-encoder/src/encode.rs
+++ dag-encoder/src/encode.rs
@@ -1,4 +1,23 @@
 use crate::Graph;
-use std::io::Write;
+use std::io::{self, Write};
 
-pub fn encode(_graph: &Graph, _writer: &impl Write) {}
+pub fn encode(_graph: &Graph, _writer: &impl Write) -> Result<(), io::Error> {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use insta::assert_debug_snapshot;
+
+    fn encode_to_vec(graph: &Graph) -> Vec<u8> {
+        let mut buffer = vec![];
+
+        encode(&graph, &mut buffer).unwrap();
+
+        buffer
+    }
+
+    #[test]
+    fn encode_empty() {
+        assert_debug_snapshot!(encode_to_vec(&Graph::default()));
+    }
+}
