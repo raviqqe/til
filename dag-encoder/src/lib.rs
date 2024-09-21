3ce3827f4fb77f5079c557f6e70c154ead1f4b3b
Yota Toyama <raviqqe@gmail.com>
2024-09-21 18:43:10 +0900

    Fix

--- dag-encoder/src/lib.rs
+++ dag-encoder/src/lib.rs
@@ -16,7 +16,7 @@ pub use payload::Payload;
 
 const INTEGER_BASE: u128 = 1 << 7;
 const FIXED_LINK_PAYLOAD_BASE: u128 = 1 << 2;
-const VARIADIC_LINK_TYPE: usize = 5;
+const VARIADIC_LINK_TYPE: usize = 4;
 
 #[cfg(test)]
 mod tests {
@@ -97,7 +97,7 @@ mod tests {
         )));
     }
 
-    macro_rules! test_payload {
+    macro_rules! test_link_payload {
         ($name:ident, $payload:literal) => {
             #[test]
             fn $name() {
@@ -113,8 +113,33 @@ mod tests {
         };
     }
 
-    test_payload!(zero_payload, 0.0);
-    test_payload!(one_payload, 1.0);
-    test_payload!(two_payload, 2.0);
-    test_payload!(positive_integer_payload, 42.0);
+    test_link_payload!(zero_link_payload, 0.0);
+    test_link_payload!(one_link_payload, 1.0);
+    test_link_payload!(two_link_payload, 2.0);
+    test_link_payload!(positive_integer_link_payload, 42.0);
+
+    macro_rules! test_link_type {
+        ($name:ident, $type:literal) => {
+            #[test]
+            fn $name() {
+                assert_encode_decode!(Graph::new(Some(
+                    Node::Link {
+                        r#type: $type,
+                        payload: Payload::Number(0.0),
+                        next: None
+                    }
+                    .into()
+                )));
+            }
+        };
+    }
+
+    test_link_type!(zero_link_type, 0);
+    test_link_type!(one_link_type, 1);
+    test_link_type!(two_link_type, 2);
+    test_link_type!(three_link_type, 3);
+    test_link_type!(four_link_type, 4);
+    test_link_type!(five_link_type, 5);
+    test_link_type!(six_link_type, 6);
+    test_link_type!(positive_integer_link_type, 42);
 }
