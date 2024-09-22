1b657f85ec9e53d5771488b90e6ea72a260fdc59
Yota Toyama <raviqqe@gmail.com>
2024-09-22 20:33:17 +0900

    Encode left link

--- dag-encoder/src/encode.rs
+++ dag-encoder/src/encode.rs
@@ -36,6 +36,37 @@ pub fn encode(graph: &Graph, mut writer: impl Write) -> Result<(), Error> {
     Ok(())
 }
 
+fn encode_node(node: &Node, writer: &mut impl Write) -> Result<(), Error> {
+    let mut node = node;
+
+    while let Node::Link(link) = node {
+        let r#type = link.r#type();
+        let r#return = link.right().is_value() as u8;
+        let Node::Value(value) = link.left() else {
+            panic!("merge not supported")
+        };
+
+        if r#type < VARIADIC_LINK_TYPE {
+            let integer =
+                encode_integer_with_base(encode_value(*value), FIXED_LINK_PAYLOAD_BASE, writer)?;
+
+            writer.write_all(&[(integer << 5) | ((r#type as u8) << 3) | r#return])?;
+        } else {
+            let r#type = r#type - VARIADIC_LINK_TYPE;
+
+            encode_integer(encode_value(*value), writer)?;
+            let integer =
+                encode_integer_with_base(r#type as _, VARIADIC_LINK_PAYLOAD_BASE, writer)?;
+
+            writer.write_all(&[(integer << 3) | (1 << 2) | r#return])?;
+        }
+
+        node = link.right();
+    }
+
+    Ok(())
+}
+
 fn encode_value(value: f64) -> u128 {
     if value.fract() != 0.0 {
         panic!("floating point value not supported")
