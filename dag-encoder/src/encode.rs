e64c340931e67f7d796f35ac993c563bb23fe1b0
Yota Toyama <raviqqe@gmail.com>
2024-09-22 19:07:47 +0900

    Fix

--- dag-encoder/src/encode.rs
+++ dag-encoder/src/encode.rs
@@ -46,7 +46,7 @@ pub fn encode(graph: &Graph, mut writer: impl Write) -> Result<(), Error> {
 fn encode_payload(payload: &Payload) -> u128 {
     match payload {
         Payload::Node(_node) => {
-            todo!()
+            panic!("node payload not supported")
         }
         &Payload::Number(number) => {
             if number.fract() != 0.0 {
