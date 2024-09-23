c1c97ae7d52b1b7deff6335f25d3695d49645c11
Yota Toyama <raviqqe@gmail.com>
2024-09-23 15:36:27 +0900

    Fix

--- dag-encoder/src/encode.rs
+++ dag-encoder/src/encode.rs
@@ -19,7 +19,7 @@ fn encode_node(node: &Node, writer: &mut impl Write) -> Result<(), Error> {
             }
             Node::Value(value) => {
                 let integer = encode_integer_with_base(encode_value(*value), VALUE_BASE, writer)?;
-                writer.write_all(&[(integer << 1) | 0])?;
+                writer.write_all(&[(integer << 1)])?;
                 return Ok(());
             }
         };
