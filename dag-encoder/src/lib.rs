04968a559da843b7e8876b1795b852e76ce1a20e
Yota Toyama <raviqqe@gmail.com>
2024-09-23 18:42:01 +0900

    Fix

--- dag-encoder/src/lib.rs
+++ dag-encoder/src/lib.rs
@@ -17,7 +17,7 @@ pub use node::Node;
 const INTEGER_BASE: u128 = 1 << 7;
 const VALUE_BASE: u128 = 1 << 6;
 const TYPE_BASE: u128 = 1 << 5;
-const SHARE_BASE: u128 = 1 << 5 - 1;
+const SHARE_BASE: u128 = (1 << 5) - 1;
 
 #[cfg(test)]
 mod tests {
