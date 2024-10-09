e8ca9b676b07410659b13b1dbbe3bdcf73358b2c
Yota Toyama <raviqqe@gmail.com>
2024-10-09 23:52:48 +0900

    Fix single share

--- dag-encoder/src/lib.rs
+++ dag-encoder/src/lib.rs
@@ -27,14 +27,16 @@ mod tests {
     use pretty_assertions::assert_eq;
 
     macro_rules! assert_encode_decode {
-        ($graph:expr) => {
+        ($graph:expr) => {{
             let graph = $graph;
             let mut buffer = vec![];
 
             encode(&graph, &mut buffer).unwrap();
 
             assert_eq!(&decode(&*buffer).unwrap(), &graph);
-        };
+
+            graph
+        }};
     }
 
     macro_rules! assert_decode_error {
