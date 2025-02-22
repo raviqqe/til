2c961e38a979e9b17cbf1e0f82e3ba99d5d477f7
Yota Toyama <raviqqe@gmail.com>
2025-02-22 09:21:34 +0900

    Fix

--- rust-libc/src/main.rs
+++ rust-libc/src/main.rs
@@ -3,7 +3,9 @@
 
 #[cfg_attr(not(test), unsafe(no_mangle))]
 unsafe extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    unsafe { libc::printf(c"Hello, World!".as_ptr() as _) }
+    unsafe {
+        libc::printf(c"Hello, World!".as_ptr() as _);
+    }
 
     0
 }
