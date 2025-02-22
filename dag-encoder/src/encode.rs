45c9422be86d717150348d2df1655bb93c31762b
Yota Toyama <raviqqe@gmail.com>
2025-02-22 09:12:51 +0900

    Update Rust edition (#307)

--- dag-encoder/src/encode.rs
+++ dag-encoder/src/encode.rs
@@ -26,7 +26,7 @@ fn encode_node(
                         SHARE_BASE,
                     );
 
-                    writer.write_all(&[(head + 1) << 2 | 0b11])?;
+                    writer.write_all(&[((head + 1) << 2) | 0b11])?;
                     encode_integer_tail(tail, writer)?;
                     return Ok(());
                 }
@@ -37,7 +37,7 @@ fn encode_node(
 
             let (head, tail) = encode_integer_parts(link.r#type() as _, TYPE_BASE);
 
-            writer.write_all(&[head << 2 | 1])?;
+            writer.write_all(&[(head << 2) | 1])?;
             encode_integer_tail(tail, writer)?;
 
             if link.share().is_some() {
