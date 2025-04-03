cce91634d84ad5932ad031f46d6e765b09304af8
Yota Toyama <raviqqe@gmail.com>
2025-04-03 16:31:29 -0700

    Test traits

---
+++ rust-ref-ref-trait/src/main.rs
@@ -0,0 +1,16 @@
+use std::rc::Rc;
+
+fn main() {}
+
+trait Foo {
+    fn foo(foo) {
+        println("Hello");
+    }
+}
+
+trait RefTrait {
+    fn foo(foo) {
+        println("Hello");
+    }
+
+}
