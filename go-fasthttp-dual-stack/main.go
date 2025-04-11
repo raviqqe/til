b64843529e8e36437636a3d3252e705b943e1601
Yota Toyama <raviqqe@gmail.com>
2025-04-10 18:02:31 -0700

    Fix

---
+++ go-fasthttp-dual-stack/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/valyala/fasthttp"
+)
+
+const concurrency = 1
+const address = "http://localhost:8000"
+
+func main() {
+	client := fasthttp.Client{DialDualStack: true}
+	cs := make(chan struct{}, concurrency)
+	es := make(chan error)
+
+	go func() {
+		for {
+			cs <- struct{}{}
+
+			go func() {
+				bs := []byte{}
+				_, _, err := client.Get(bs, address)
+				es <- err
+				<-cs
+			}()
+		}
+	}()
+
+	for e := range es {
+		fmt.Println(e)
+	}
+
+	// Now, we see the following. But why?
+	//
+	// ```
+	// <nil>
+	// error when dialing [::1]:8000: dial tcp [::1]:8000: connect: connection refused
+	// <nil>
+	// error when dialing [::1]:8000: dial tcp [::1]:8000: connect: connection refused
+	// ...
+	// ```
+}
