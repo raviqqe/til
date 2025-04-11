28c2bc8357bce176ef509dbecc76dda6c2022adf
Yota Toyama <raviqqe@gmail.com>
2025-04-11 10:18:07 +0900

    `fasthttp` dual stack connections (#346)
    This example reproduces https://github.com/raviqqe/muffet/issues/449.

---
+++ go-fasthttp-dual-stack/go.mod
@@ -0,0 +1,11 @@
+module github.com/raviqqe/til/go-fasthttp-dual-stack
+
+go 1.24.2
+
+require github.com/valyala/fasthttp v1.60.0
+
+require (
+	github.com/andybalholm/brotli v1.1.1 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+)
---
+++ go-fasthttp-dual-stack/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/valyala/fasthttp"
+)
+
+const address = "http://localhost:8000"
+
+func main() {
+	c := fasthttp.Client{DialDualStack: true}
+	es := make(chan error)
+
+	go func() {
+		for {
+			_, _, err := c.Get([]byte{}, address)
+			es <- err
+
+			time.Sleep(time.Second)
+		}
+	}()
+
+	for e := range es {
+		fmt.Println(e)
+	}
+
+	// Now, we see the following. But why?
+	//
+	// ```
+	// <nil>
+	// error when dialing [::1]:8000: dial tcp [::1]:8000: connect: connection refused
+	// <nil>
+	// error when dialing [::1]:8000: dial tcp [::1]:8000: connect: connection refused
+	// ...
+	// ```
+}
