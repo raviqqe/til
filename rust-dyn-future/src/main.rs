72bf673c1fb4454fd3f9ad2a51cde92c9fc5b2ae
Yota Toyama <raviqqe@gmail.com>
2025-04-22 17:21:15 -0700

    Fix

--- rust-dyn-future/src/main.rs
+++ rust-dyn-future/src/main.rs
@@ -1,4 +1,18 @@
+use allocator_api2::{
+    alloc::{Allocator, Global},
+    boxed::Box,
+};
+use core::pin::Pin;
+
+fn foo<A: Allocator>(
+    allocator: A,
+    future: impl Future<Output = isize> + 'static,
+) -> Pin<Box<dyn Future<Output = isize> + 'static, A>> {
+    Box::pin_in(future, allocator)
+}
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let future = foo(Global, async { 42 });
+    println!("value: {}", future.await);
 }
