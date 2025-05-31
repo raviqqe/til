a0e4096692137abbe0a37adb70fcf3967dcf10cb
Yota Toyama <raviqqe@gmail.com>
2025-05-31 16:10:54 -0700

    Fix

--- stak-benchmark-result/src/index.ts
+++ stak-benchmark-result/src/index.ts
@@ -3,6 +3,22 @@ import { join } from "path";
 import { object, array, parse, number, string } from "valibot";
 import { joinBlocks, table } from "ts-markdown-builder";
 
+const commands = [
+  "stak",
+  "mstak",
+  "stak-interpret",
+  "mstak-interpret",
+  "chibi-scheme",
+  "gosh",
+  "guile",
+  "gsi",
+  "python3",
+  "micropython",
+  "ruby",
+  "mruby",
+  "lua",
+];
+
 const benchmarkSchema = object({
   results: array(
     object({
@@ -18,26 +34,33 @@ if (!directory) {
   throw new Error("directory argument not defined");
 }
 
+const readResults = (path: string) => {
+  const data = parse(
+    benchmarkSchema,
+    JSON.parse(await readFile(join(directory, path), "utf-8")),
+  );
+
+  return Object.fromEntries(
+    data.results.map(({ command, median }) => [
+      command.split(" ")[0] ?? command,
+      median,
+    ]),
+  );
+};
+
 for (const path of await readdir(directory)) {
   if (!path.endsWith(".json")) {
     continue;
   }
 
-  const data = parse(
-    benchmarkSchema,
-    JSON.parse(await readFile(join(directory, path), "utf-8")),
-  );
-  const results = data.results.map(({ command, ...rest }) => ({
-    command: command.split(" ")[0] ?? command,
-    ...rest,
-  }));
-
-  console.log(
-    joinBlocks([
-      table(
-        results.map(({ command }) => command),
-        [results.map(({ median }) => median.toString())],
-      ),
-    ]),
-  );
+  const results = results;
 }
+
+console.log(
+  joinBlocks([
+    table(
+      ["Benchmark", ...results.map(({ command }) => command)],
+      [results.map(({ median }) => median.toString())],
+    ),
+  ]),
+);
