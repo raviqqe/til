98c8b1da9ce5451838a8f725beb0ac3c04f2d942
Yota Toyama <raviqqe@gmail.com>
2025-05-31 18:21:17 -0700

    Fix

---
+++ stak-benchmark-result/src/tex.ts
@@ -0,0 +1,49 @@
+import { table, tsMarkdown } from "ts-markdown";
+import { readBenchmarks } from "./benchmark.js";
+
+const referenceCommand = "mstak-interpret";
+const commands = [
+  referenceCommand,
+  "stak-interpret",
+  "mstak",
+  "stak",
+  "gsi",
+  "chibi-scheme",
+  "gosh",
+  "guile",
+  "python3",
+  "micropython",
+  "ruby",
+  "mruby",
+  "lua",
+];
+
+const fractionDigits = 2;
+const numberFormat = new Intl.NumberFormat(undefined, {
+  maximumFractionDigits: fractionDigits,
+  minimumFractionDigits: fractionDigits,
+});
+
+const [, , directory] = process.argv;
+
+if (!directory) {
+  throw new Error("directory argument not defined");
+}
+
+const benchmarks = await readBenchmarks(directory, referenceCommand);
+
+console.log(
+  tsMarkdown([
+    table({
+      columns: ["Benchmark", ...commands],
+      rows: benchmarks.map(([name, results]) => [
+        name,
+        ...commands.map((command) =>
+          results[command] === undefined
+            ? "-"
+            : numberFormat.format(results[command]),
+        ),
+      ]),
+    }),
+  ]),
+);
