13b34c68d4150eeb9a46d072fe7ae0015a3ba295
Yota Toyama <raviqqe@gmail.com>
2025-08-13 19:31:07 -0700

    Fix

--- pair-vm/src/vm.rs
+++ pair-vm/src/vm.rs
@@ -1,9 +1,12 @@
 use core::convert::{AsMut, AsRef};
 
-pub struct Vm<M: AsRef<[u64]> + AsMut<[u64]>> {
-    memory: M,
+pub struct Vm<T: AsRef<[u64]> + AsMut<[u64]>> {
+    register: u64,
+    heap: T,
 }
 
-impl<M: AsRef<[u64]> + AsMut<[u64]> Vm<M> {
-    fn foo() {}
+impl<T: AsRef<[u64]> + AsMut<[u64]>> Vm<T> {
+    pub fn run(&mut self) {
+        foo;
+    }
 }
