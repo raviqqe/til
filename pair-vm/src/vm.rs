6f9f84cc8d29d2b019fec8a0b626d7071553b02f
Yota Toyama <raviqqe@gmail.com>
2025-08-13 19:38:48 -0700

    Fix

--- pair-vm/src/vm.rs
+++ pair-vm/src/vm.rs
@@ -1,18 +1,21 @@
-use core::convert::{AsMut, AsRef};
+use core::ops::{Deref, DerefMut};
 
-#[expect(dead_code)]
-pub struct Vm<T: AsRef<[u64]> + AsMut<[u64]>> {
+type Heap = [u64];
+
+pub struct Vm<T: Deref<Target = Heap> + DerefMut<Target = Heap>> {
     register: u64,
     heap: T,
 }
 
-impl<T: AsRef<[u64]> + AsMut<[u64]>> Vm<T> {
+impl<T: Deref<Target = Heap> + DerefMut<Target = Heap>> Vm<T> {
     pub fn new(heap: T) -> Self {
         Vm { register: 0, heap }
     }
 
     pub fn run(&mut self) -> Result<(), &'static str> {
-        while let Some(cons) = self.get().to_cons() {}
+        while let Some(value) = self.heap.get(self.register as usize) {
+            println!("{}", value);
+        }
 
         Ok(())
     }
