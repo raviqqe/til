26fd296d24981ffab5eddd9562b0bbc154da5ea3
Yota Toyama <raviqqe@gmail.com>
2025-08-23 14:33:33 -0700

    Benchmark table v2 (#471)

--- stak-benchmark-result/src/tex.ts
+++ stak-benchmark-result/src/tex.ts
@@ -2,7 +2,8 @@ import { parseArgs } from "node:util";
 import { readBenchmarks } from "./benchmark.ts";
 
 const referenceCommand = "mstak";
-const commands = [
+
+const mixedCommands = [
   referenceCommand,
   "stak",
   "tr7i",
@@ -13,7 +14,18 @@ const commands = [
   "ruby",
   "mruby",
 ];
-const benchmarkNames = ["fibonacci", "sum", "tak"];
+
+const schemeCommands = [
+  referenceCommand,
+  "stak",
+  "mstak-interpret",
+  "stak-interpret",
+  "gsi",
+  "chibi-scheme",
+  "gosh",
+];
+
+const benchmarkNames = ["empty", "fibonacci", "hello", "sum", "tak"];
 
 const fractionDigits = 2;
 const numberFormat = new Intl.NumberFormat(undefined, {
@@ -26,13 +38,23 @@ const printRow = (row: string[]) => console.log(`${row.join(" & ")} \\\\`);
 
 const {
   positionals: [directory],
-} = parseArgs({ allowPositionals: true });
+  values: { scheme },
+} = parseArgs({
+  allowPositionals: true,
+  options: {
+    scheme: {
+      type: "boolean",
+    },
+  },
+});
 
 if (!directory) {
   throw new Error("directory argument not defined");
 }
 
 const benchmarks = await readBenchmarks(directory, referenceCommand);
+
+const commands = scheme ? schemeCommands : mixedCommands;
 
 printLine();
 printRow(["Benchmark", ...commands]);
