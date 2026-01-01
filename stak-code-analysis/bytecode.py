009b9eb11c8a97cc753c2381a689ace226547463
Yota Toyama <raviqqe@gmail.com>
2026-01-01 15:28:57 -0800

    Fix

--- stak-code-analysis/bytecode.py
+++ stak-code-analysis/bytecode.py
@@ -4,9 +4,19 @@ import polars
 import matplotlib.pyplot
 
 
+bits = 8
+
+
 def main():
     with open(sys.argv[1], "rb") as file:
         frame = polars.DataFrame({"code": list(file.read())})
+
+    frame2 = polars.DataFrame(
+        {
+            "code": frame["code"].repeat_by(bits),
+            "bit": polars.lit(range(8)),
+        }
+    )
 
     seaborn.displot(frame, x="code", discrete=True)
     seaborn.displot(frame, x="bit", hue="flag", discrete=True)
