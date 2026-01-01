c5e325f269410e238d425058e413c7f0a174b6ca
Yota Toyama <raviqqe@gmail.com>
2026-01-01 00:18:56 -0800

    Analyze bytecode distribution (#595)

---
+++ stak-code-analysis/bytecode.py
@@ -0,0 +1,17 @@
+import seaborn
+import sys
+import pandas
+import matplotlib.pyplot
+
+
+def main():
+    with open(sys.argv[1], "rb") as file:
+        code = pandas.DataFrame({"code": [byte for byte in file.read()]})
+
+    seaborn.displot(code, x="code")
+
+    matplotlib.pyplot.show()
+
+
+if __name__ == "__main__":
+    main()
