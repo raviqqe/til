912649f4bdcdc25bb48470f4868bcf6178847cbd
Yota Toyama <raviqqe@gmail.com>
2026-01-01 15:51:25 -0800

    Fix

--- stak-code-analysis/bytecode.py
+++ stak-code-analysis/bytecode.py
@@ -13,7 +13,7 @@ def main():
         xs = numpy.array(list(file.read()))
 
     # Turn into a 2D array with 3 columns
-    xs = numpy.stack([xs, xs // 1 == 0], axis=1)
+    xs = numpy.stack([xs, *(xs // (1 << i) == 0 for i in range(bits))], axis=1)
 
     frame = polars.DataFrame(xs)
 
