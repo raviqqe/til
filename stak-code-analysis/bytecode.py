d356bb219a2672c979767355a72420707295f77a
Yota Toyama <raviqqe@gmail.com>
2026-01-01 15:50:09 -0800

    Fix

--- stak-code-analysis/bytecode.py
+++ stak-code-analysis/bytecode.py
@@ -1,6 +1,7 @@
 import seaborn
 import sys
 import matplotlib.pyplot
+import polars
 import numpy
 
 
@@ -9,12 +10,19 @@ bits = 8
 
 def main():
     with open(sys.argv[1], "rb") as file:
-        array = numpy.array(list(file.read()))
+        xs = numpy.array(list(file.read()))
 
-    seaborn.displot(array, x=0, discrete=True)
+    # Turn into a 2D array with 3 columns
+    xs = numpy.stack([xs, xs // 1 == 0], axis=1)
+
+    frame = polars.DataFrame(xs)
+
+    print(frame)
+
+    seaborn.displot(frame, x="column_0", discrete=True)
     # seaborn.displot(array, x=0, hue=2, discrete=True)
 
-    matplotlib.pyplot.show()
+    # matplotlib.pyplot.show()
 
 
 if __name__ == "__main__":
