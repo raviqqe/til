b4f238107416717df4e664034cd3f1c6bf257bf5
Yota Toyama <raviqqe@gmail.com>
2026-01-01 16:05:34 -0800

    Fix

--- stak-code-analysis/bytecode.py
+++ stak-code-analysis/bytecode.py
@@ -12,7 +12,8 @@ def main():
     with open(sys.argv[1], "rb") as file:
         xs = numpy.array(list(file.read()))
 
-    ys = numpy.stack([xs & (1 << bit) > 0 for bit in range(bits)])
+    ys = numpy.stack([xs & (1 << bit) > 0 for bit in range(bits)]).transpose()
+    zs = numpy.array([])
 
     frame = polars.DataFrame({"code": xs, "bit": ys})
 
