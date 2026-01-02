19ab9c192b44ff252ba637b108b8b56e3708d794
Yota Toyama <raviqqe@gmail.com>
2026-01-01 16:01:43 -0800

    Fix

--- stak-code-analysis/bytecode.py
+++ stak-code-analysis/bytecode.py
@@ -12,7 +12,7 @@ def main():
     with open(sys.argv[1], "rb") as file:
         xs = numpy.array(list(file.read()))
 
-    ys = numpy.stack([xs & (1 << bit) > 0 for bit in range(bits)]).transpose()
+    ys = numpy.stack([xs & (1 << bit) > 0 for bit in range(bits)])
 
     frame = polars.DataFrame({"code": xs, "bit": ys})
 
@@ -22,7 +22,7 @@ def main():
     seaborn.displot(frame, x="code", discrete=True)
     # seaborn.displot(array, x=0, hue=2, discrete=True)
 
-    # matplotlib.pyplot.show()
+    matplotlib.pyplot.show()
 
 
 if __name__ == "__main__":
