798fb52931b9a8eb5c65353168055e3fd027c49c
Yota Toyama <raviqqe@gmail.com>
2026-01-21 18:56:43 -0800

    Python property set

---
+++ python-property-set/main.py
@@ -0,0 +1,15 @@
+def canada():
+    return {}
+
+
+def japan():
+    print("Hello from python-property-set!")
+
+
+def main():
+    print(canada())
+    print(japan())
+
+
+if __name__ == "__main__":
+    main()
