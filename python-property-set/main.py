cb5a0813ae3cea17d09e87b2ea4d36f0ee6398c5
Yota Toyama <raviqqe@gmail.com>
2026-01-21 19:09:12 -0800

    Fix

--- python-property-set/main.py
+++ python-property-set/main.py
@@ -1,14 +1,29 @@
-def canada():
+from pprint import pprint
+
+
+def c():
     return {}
 
 
-def japan():
-    print("Hello from python-property-set!")
+def j():
+    x = 850
+    d = 20 * 3
+
+    p = {
+        "j_nk": 60,
+        "j_sc": 15,
+        "w_bd": 5,
+        "w_ac": 15,
+        "w_dc": 5,
+    }
+
+    assert sum(p.values()) == 100
+
+    return {k: v / 100 * x / d for k, v in p.items()}
 
 
 def main():
-    print(canada())
-    print(japan())
+    pprint({"c": c(), "j": j()})
 
 
 if __name__ == "__main__":
