2ed7e5aee916846bfbdefd66dd9659ddbaf32c0e
Yota Toyama <raviqqe@gmail.com>
2026-01-30 23:31:04 -0800

    Update pp (#632)
    - **Fix**
    - **Fix**
    - **Fix**
    - **Fix**

--- python-property-set/property_set/__init__.py
+++ python-property-set/property_set/__init__.py
@@ -1,8 +1,27 @@
+# spell-checker: ignore veqt
+
 w = 5
 
 
 def c():
-    return {}
+    x = 100_000
+    d = 20
+
+    p = {
+        "veqt.to": 50,
+        "tec.to": 30,
+        "qqc.to": 15,
+        "cash.to": 5,
+    }
+
+    assert sum(p.values()) == 100
+
+    ps = {k: v / 100 * x for k, v in p.items()}
+
+    return {
+        "t": ps,
+        "d": {k: v / d for k, v in ps.items()},
+    }
 
 
 def j():
@@ -13,16 +32,19 @@ def j():
     p = {
         "j_nk": 60,
         "j_sc": 15,
-        "w_bd": 5,
         "w_ac": 15,
         "w_dc": 5,
+        "w_bd": 5,
     }
 
     assert sum(p.values()) == 100
 
-    ps = {k: v / 100 * x / d for k, v in p.items()}
+    ps = {k: v / 100 * x for k, v in p.items()}
 
     return {
-        "ps": ps,
-        "j_sc_p": ps["j_sc"] / j_sc_p * w,
+        "d": {k: v / d for k, v in ps.items()},
+        "t": ps,
+        "z": {
+            "j_sc_p": ps["j_sc"] / j_sc_p * w,
+        },
     }
