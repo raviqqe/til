5b1c9f4bb4732279dc1f383d1ba8605130c5f3dc
Yota Toyama <raviqqe@gmail.com>
2026-02-02 09:15:59 -0800

    Dump JSON (#636)
    - **Fix**
    - **Fix**
    - **Fix**

---
+++ python-property/main.py
@@ -0,0 +1,20 @@
+import sys
+import json
+import prop
+
+
+def main():
+    json.dump(
+        {
+            "c": prop.c(),
+            "c15": prop.c(15_000),
+            "j": prop.j(),
+        },
+        sys.stdout,
+        indent=2,
+        sort_keys=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
